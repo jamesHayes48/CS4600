9f8f6eb81729a188c3464b0a9db0c4199c76c2f4
jamesHayes48 <jhpipboy@gmail.com>
2025-09-09 10:36:00 -0600

    Update Question-4.py

--- Homework 1/Question-4.py
+++ Homework 1/Question-4.py
@@ -4,6 +4,7 @@ import numpy as np
 def function(x):
     return -12 - 21*x + 18*x**2 - 2.75*x**3
 
+
 def bisection_method(f, num_iter, lower_limit, upper_limit):
     interval = [lower_limit, upper_limit]
     midpoint = 0
@@ -19,12 +20,12 @@ def bisection_method(f, num_iter, lower_limit, upper_limit):
         decide = f(interval[0]) * func_midpoint
 
         if decide < 0:
-            interval[0] = midpoint
-        elif decide > 0:
             interval[1] = midpoint
+        elif decide > 0:
+            interval[0] = midpoint
 
         print(f"{n}: x_{n} = {midpoint} f(x_r) = {func_midpoint} f(x_l) * f(x_{n}) = {decide} "
-              f"new interval: {interval} E_a = {error_midpoint}%")
+              f"new interval: {interval} Error = {error_midpoint}%")
         old_midpoint = midpoint
 
 def false_position(f, num_iter, lower_limit, upper_limit):
@@ -32,7 +33,6 @@ def false_position(f, num_iter, lower_limit, upper_limit):
     old_midpoint = 0
 
     for n in range(num_iter):
-        print(f(interval[0]))
         x_r = ((f(interval[0])*interval[1] - f(interval[1])*interval[0]) /
                (f(interval[0]) - f(interval[1])))
         function_x_r = f(x_r)
@@ -43,7 +43,7 @@ def false_position(f, num_iter, lower_limit, upper_limit):
             interval[0] = x_r
         elif function_x_r > 0:
             interval[1] = x_r
-        print(f"x_{n} = {x_r} f(x_r) = {function_x_r} new_interval: {interval} E_a = {error_midpoint}")
+        print(f"x_{n} = {x_r} f(x_r) = {function_x_r} new_interval: {interval} Error = {error_midpoint}")
         old_midpoint = x_r
 
 x_list = np.linspace(-2, 5)
@@ -55,5 +55,8 @@ plt.axvline(0, color='black')
 plt.show()
 
 # Run the bisection and false-position method 20 times
+print("Bisection Method: ")
 bisection_method(function, num_iter=20, lower_limit=-1, upper_limit=0)
+
+print("\nFalse-Position Method: ")
 false_position(function, num_iter=20, lower_limit=-1, upper_limit=0)
