a63501f4cf320e348b539cab43d304395aadcbac
DingDongReallyFast <jhpipboy@gmail.com>
2025-09-11 16:07:32 -0600

    Update Question-6.py

--- Homework 1/Question-6.py
+++ Homework 1/Question-6.py
@@ -1,8 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import sympy as sp
 
+
+x = sp.symbols('x')
 def function(x):
     return x**3 - 6*x**2 + 11*x -6.1
+
+
+def newton_method(f, starting_point, num_iter):
+    fxn = x**3 - 6*x**2 + 11*x - 6.1
+
+    # Get derivative of f
+    deriv_f = sp.Derivative(fxn, x).doit()
+    print(type(deriv_f))
+
 
 
 # a. Graph the function over [0,5]
@@ -13,3 +25,6 @@ plt.plot(x_list, y_list)
 plt.axhline(0, color='black')
 plt.axvline(0, color='black')
 plt.show()
+
+# c. Find real root with Newton Method
+newton_method(function, 3.5, 3)
