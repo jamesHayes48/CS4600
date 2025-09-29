8d5f3db6829648de683a155e54b43b015ed7cc3f
jamesHayes48 <jhpipboy@gmail.com>
2025-09-29 08:01:44 -0600

    Update Question-4.py

--- Homework 2/Question-4.py
+++ Homework 2/Question-4.py
@@ -1,10 +1,12 @@
 '''
 Question 6c - Implement steepest descent method in Python.
+Print its contour and path of optimization
 '''
 
 import sympy as sp
+import numpy as np
+import matplotlib.pyplot as plt
 import math
-from sympy.physics.quantum.operatorordering import normal_ordered_form
 
 x, y = sp.symbols('x y')
 
@@ -13,49 +15,72 @@ def steep_descent(f, h, starting_x, starting_y):
     df_dx = sp.diff(f, x)
     df_dy = sp.diff(f, y)
 
-    # Set first starting points
+    # Set first starting points to x_i
     x_i = starting_x
     y_i = starting_y
 
-    substitute_dict = {x: x_i, y: y_i}
+    # Store norm_gradient
+    norm_gradient = 100
 
-    dx_sub = df_dx.subs(substitute_dict).evalf()
-    dy_sub = df_dy.subs(substitute_dict).evalf()
+    # Store starting points
+    x_array = [starting_x]
+    y_array = [starting_y]
 
-
-    # Find first norm of the gradient with starting point
-    norm_gradient = abs(math.sqrt(((dx_sub ** 2) + (dy_sub ** 2))))
-
-    opt_array = [(starting_x, starting_y)]
+    # Store number of iterations
+    num_itr = 0
 
     while norm_gradient > 10 ** -4:
         # Substitute the points of iteration
-        dx_sub = df_dx.subs((x, x_i ), (y, y_i))
-        dy_sub = df_dy.subs((x, x_i ), (y, y_i))
-
         substitute_dict = {x: x_i, y: y_i}
-
         dx_sub = df_dx.subs(substitute_dict).evalf()
         dy_sub = df_dy.subs(substitute_dict).evalf()
-        print(dy_sub)
+
         # Find new points
         x_i = x_i - (h * dx_sub)
         y_i = y_i - (h * dy_sub)
 
         # Find norm of the gradient to continue
-        norm_gradient = abs(math.sqrt(((dx_sub ** 2) + (dy_sub ** 2))))
+        norm_gradient = math.sqrt(((dx_sub ** 2) + (dy_sub ** 2)))
 
-        # Add to optimization array
-        opt_array.append((x_i, y_i))
+        # Add x to x_array
+        # Add y to y_array
+        x_array.append(x_i)
+        y_array.append(y_i)
+        num_itr += 1
 
-    return opt_array
+    print(f"Number of Iterations: {num_itr}")
+    return x_array, y_array
 
 
-fxn = sp.sin(x) + sp.cos(y) + ((x - y)**2) / 4
+# Symbolic function for sympy and numeric for numpy
+fxn_sym = sp.sin(x) + sp.cos(y) + ((x - y)**2) / 4
+fxn_num = sp.lambdify((x,y), fxn_sym, 'numpy')
+
+# Define step size and starting points
 step_size = 0.2
 x_0 = 1
 y_0 = 2
-optimization =  steep_descent(fxn, step_size, x_0, y_0)
-plot1 = sp.plotting.plot3d(fxn, (x, -10, 10), (y, -10, 10))
-plot2 = sp.plotting.plot3d(optimization)
-plot1.extend(plot2)
+
+# Find optimization path, store them into arrays for x and y
+opt_x, opt_y =  steep_descent(fxn_sym, step_size, x_0, y_0)
+
+# Plots for contour graph of function
+x_list = np.linspace(-2, 8, 100)
+y_list = np.linspace(-2, 8, 100)
+
+X,Y = np.meshgrid(x_list, y_list)
+Z = fxn_num(X, Y)
+
+#
+fig, ax = plt.subplots(figsize=(9,7))
+
+# Plot contour of f(x,y)
+plt.contour(X,Y,Z)
+
+# Plot path of optimization in red
+ax.plot(opt_x, opt_y, color='red')
+
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+plt.show()
