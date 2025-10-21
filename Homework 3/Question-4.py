5e9b1a314f009f95900ba3701030ef8edb976f9d
DingDongReallyFast <jhpipboy@gmail.com>
2025-10-21 16:43:13 -0600

    Update Question-4.py

--- Homework 3/Question-4.py
+++ Homework 3/Question-4.py
@@ -10,19 +10,19 @@ Qa = 150 # m^3 / hr
 ca = 1 # mg / m^3
 Qb = 50 # m^3 / hr
 cb = 40 # mg / m^3
-Qc = 50 # m^3 / hr
 Q12 = 50 # m^3 / hr
-Q13 = 50 # m^3 / hr
-Q23 = 50 # m^3 / hr
-Q34 = 90 # m^3 / hr
+E13 = 50 # m^3 / hr
+E23 = 50 # m^3 / hr
+Q3OUT = 50 # m^3 / hr
+E34 = 90 # m^3 / hr
 Load = 5000 # mg / hr
 
 A = np.array([
-            [- (-Q12 - Q13), 0, Q13, 0],
-            [-(Q12), Q23, -Q23, 0],
-            [-Q13, -Q23, -(Q23 - Q13 + Q34), -Q34],
-            [0, 0, 0, Q34]
-            ])
+    [(Q12 + E13 + Qa), 0, -E13, 0],
+    [-Q12, (Qb + Q12 + E23), -E23, 0],
+    [(Qa + E13), (Qb + Q12 + E23), -(Q3OUT + Qb + Q12 + Qa + E13 + E23 + E34), E34],
+    [0, 0, -(Qb + Q12 + Qa + E34), (Qb + Q12 + Qa + E34)]
+])
 
 b = np.array([[Qa * ca],
               [Qb * cb],
@@ -32,4 +32,4 @@ b = np.array([[Qa * ca],
 
 A_inv = np.linalg.inv(A)
 
-print(f"Answer: {np.dot(A_inv, b)}")
+print(f"Answer: \n {np.dot(A_inv, b)}")
