b6c37af0cf6bacece1d500939c6a3b5c1d9735d0
jamesHayes48 <jhpipboy@gmail.com>
2025-10-22 17:23:33 -0600

    ye

--- Homework 3/Question-4.py
+++ Homework 3/Question-4.py
@@ -1,11 +1,13 @@
 '''
 Question 4
 b. Find matrix inverse and use it to calculate concentrations in the room
+c. Find new load for when room 2's concentration = 20 mg/m^3
 '''
 
 import numpy as np
 from scipy.linalg import lu, solve_triangular
 
+# b. Find Matrix inverse and calculate c1, c2, c3, and c4
 Qa = 150 # m^3 / hr
 ca = 1 # mg / m^3
 Qb = 50 # m^3 / hr
@@ -22,9 +24,9 @@ Q34 = Q23 + Q13 - Q3OUT
 
 A = np.array([
     [Q13 + E13 + Q12, 0, -E13, 0],
-    [-Q12, Q13 + E23, -E23, 0],
-    [Q13, Q23, -(Q3OUT + Q34 + E13 + E34), E34],
-    [0, 0, -Q34 - E34, E34 + Q34]
+    [-Q12, Q23 + E23, -E23, 0],
+    [Q13 + E13, Q23 + E23, -(Q3OUT + Q34 + E13 + E23 + E34), E34],
+    [0, 0, -(Q34 + E34), E34 + Q34]
 ])
 
 b = np.array([[Qa * ca],
@@ -38,8 +40,9 @@ c = np.dot(A_inv, b)
 
 print(f"Concentrations in mg/m^3: \n {c}")
 
-target_c2 = 20
-fourth_col = A_inv[:, 3].reshape(-1, 1)
-W_new = (target_c2 - c[1,0]) / fourth_col[1,0]
+# c. Find New Load when c2 = 20
+c_new = 20
+sum_vals = (A_inv[1,0] * b[0]) + (A_inv[1,1] * b[1]) + (A_inv[1,2] * b[2])
+new_load = (20 - sum_vals) / A_inv[1,3]
 
-print(f"To reach c2 = 20 mg / m^3, Load from room 4 must be: {W_new}")
+print(f"New W_Load = {new_load}")
